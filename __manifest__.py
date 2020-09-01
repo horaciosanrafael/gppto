6aeb2b6595d855ea604b9baf734d113498f0a031
horacio <horacio.cerda@fce.uncu.edu.ar>
2020-09-01 20:36:20 -0300

    Modificación presupuestaria terminada

--- __manifest__.py
+++ __manifest__.py
@@ -21,14 +21,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
-        #'views/lineaspptos.xml'
+        #'views/templates.xml',
+        'views/reporte_ppto.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
